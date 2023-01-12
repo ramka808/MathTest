df9e842e37372bbc8835a2b536c731a810ec5383
Ramil Yafarov <ramka808@yandex.ru>
2023-01-12 16:45:22 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let button = document.getElementById("button");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+  let result = 0;
+
+  let a1 = document.getElementById("q1").value;
+  let a2 = document.getElementById("q2").value;
+  let a3 = document.getElementById("q3").value;
+  let a4 = document.getElementById("q4").value;
+  let a5 = document.getElementById("q5").value;
+
+  if (a1 == 100) {
+    result++;
+  }
+
+  if (a2 == 10000) {
+    result++;
+  }
+
+  if (a3 == 0) {
+    result++;
+  }
+
+  if (a4 == 1){
+    result++
+  }
+
+  if (a5 == 9){
+    result++
+  }
+
+  alert(result)
+}
